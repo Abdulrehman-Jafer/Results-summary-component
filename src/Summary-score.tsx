2f63cba544e724b965bf42ad33f847bb1b844719
Abdulrehman-Jafer <abdulrehmanjaferworks01233@gmail.com>
2023-06-25 14:52:27 +0500

    fixed a mini bug

--- src/Summary-score.tsx
+++ src/Summary-score.tsx
@@ -1,6 +1,7 @@
 const Summaryscore = ({ bg, icon, color, category, score, bgHsl }: { bg: string, icon: string, color: string, category: string, score: number, bgHsl: string }) => {
+    //bg classes are not working properly so I added inline fallback bg
     return (
-        <section className={`flex justify-between p-5 rounded-lg bg-${bg} `} style={{ background: bgHsl }}> // classes are not working sometime that is why added fallback
+        <section className={`flex justify-between p-5 rounded-lg bg-${bg} `} style={{ background: bgHsl }}>
             <div className="flex items-center justify-center gap-4">
                 <img src={`${icon}`} alt="svg" />
                 <p className={`text-${color}`}>{category}</p>
